c20e07f0b4c172afa7f157a199a295ec3ac2be35
Amerrnath Murugan <amerrnath.21@gmail.com>
2018-08-27 14:26:07 +0530

    Removed debugger and eslint errors

--- app/components/song-item/component.js
+++ app/components/song-item/component.js
@@ -1,9 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'tr',
-  didInsertElement() {
-    debugger;
-    this._super(...arguments);
-  }
+  tagName: 'tr'
 });
--- mirage/config.js
+++ mirage/config.js
@@ -11,7 +11,7 @@ export default function() {
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-  this.get('/songs', (schema, request) => {
+  this.get('/songs', (schema) => {
     return schema.songs.all();
   });
 
@@ -21,11 +21,11 @@ export default function() {
     return song;
   });
 
-  this.get('/artists', (schema, request) => {
+  this.get('/artists', (schema) => {
     return schema.artists.all();
   });
 
-  this.get('/genres', (schema, request) => {
+  this.get('/genres', (schema) => {
     return schema.genres.all();
   });
   
